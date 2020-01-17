21ff46e8b0eac9e647b4bc73aab677c0ba2689d8
renbing <970910609@qq.com>
2020-01-17 18:05:20 +0800

    写banner分区、notice分区和推荐分区部分

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,17 +1,80 @@
+const WXAPI = require('apifm-wxapi')
+
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    
+    inputVal:"",
+    goodsRecommend:[],
+    kanjiaList:[],
+    pingtuanList:[],
+    loadingHidden:false,
+    selectCurrent:0,
+    categories:[],
+    activeCategoryId:0,
+    goods:[],
+
+    scrollTop:0,
+    loadingMoreHidden:true,
+
+    coupons:[],
+
+    curPage:1,
+    pageSize:20,
+    cateScrollTop:0
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+    //获取banners数据
+    WXAPI.banners({type:'index'}).then(res =>{
+      if (res.code == 700) {
+        wx.showModal({
+          title: '提示',
+          content: '请在后台添加 banner 轮播图片，自定义类型填写 index',
+          showCancel: false
+        })
+      } else {
+        this.setData({
+          banners: res.data
+        });
+      }
+    }).catch(function (res) {
+      wx.showToast({
+        title: res.msg,
+        icon: 'none'
+      })
+    })
+    //获取notice数据
+    WXAPI.noticeList({pageSize:5}).then( res=>{
+      if(res.code == 0){
+        this.setData({
+          noticeList:res.data
+        })
+      }
+    })
+    //获取分类category数据
+    WXAPI.goodsCategory().then(res=>{
+      if(res.code == 0){
+        this.setData({
+          categories:res.data
+        })
+      }
+    })
+    WXAPI.goods({recommendStatus:1}).then(res=>{
+        if(res.code == 0){
+          console.log(res)
+          this.setData({
+            goodsRecommend:res.data
+          })
+        }
+    })
+
   },
 
   /**
